b871794df1042352515b1c6db84e215b47440bad
Shirasa <shirasapraja@gmail.com>
2022-10-13 15:14:24 -0600

    move font entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {ThemeProvider} from '@mui/material';
 import {theme} from '../utils/theme/config.js'
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Head from 'next/head'
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import JSArticle from '../component/JSArticle.js';
 import NavBar from '../component/NavBar.js';
 
